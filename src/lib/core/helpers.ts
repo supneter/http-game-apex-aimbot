2e188be4d2be80364e93acc5749a024ec1d8e59b
tea321000 <834914152@qq.com>
2022-08-28 13:19:55 +0800

    fix helpers error

--- src/lib/core/helpers.ts
+++ src/lib/core/helpers.ts
@@ -1,4 +1,4 @@
-export function serialize<T>(item: T) {
+export function serialize(item: any) {
   return `(${Object.entries(item)
     .sort(([a], [b]) => a.localeCompare(b))
     .map(([k, v]) => `${k}=${v}`)
